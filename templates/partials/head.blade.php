3b42f59f14947c93a396d42ede9f7d12a0f25168
Chris Johnson <chrisltd@gmail.com>
2017-03-27 13:20:03 -0400

    added urls to head

--- templates/partials/head.blade.php
+++ templates/partials/head.blade.php
@@ -3,4 +3,10 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   @php(wp_head())
+
+  <script type="text/javascript">
+    window.ajaxurl = "<?php echo admin_url('admin-ajax.php'); ?>";
+    window.networkurl = "<?php echo network_site_url(); ?>";
+    window.homeurl = '<?php echo home_url(); ?>';
+  </script>
 </head>
